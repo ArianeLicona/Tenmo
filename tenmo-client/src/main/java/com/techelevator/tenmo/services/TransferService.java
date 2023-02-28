4841bfabfd0d70fe21aad9f0811474220631f2ec
Aria <ariaodeli21@gmail.com>
2023-02-27 21:49:48 -0600

    added a new TransferService class and worked on some methods in said class

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -0,0 +1,45 @@
+package com.techelevator.tenmo.services;
+
+import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+public class TransferService {
+    private static final String API_BASE_URL = "http://localhost:8080/transfer";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private AuthenticatedUser authenticatedUser;
+
+    public TransferService(AuthenticatedUser authenticatedUser){
+        this.authenticatedUser = authenticatedUser;
+    }
+
+    //method to get all past transfers. not sure if i did this right, i might add a test just to make sure.
+    public Transfer[] getPastTransfers(){
+        Transfer[] transfers = null;
+        try {
+            transfers = restTemplate.getForObject(API_BASE_URL, Transfer[].class);
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return transfers;
+    }
+
+    //not 100% sure this method has everything it needs. would appreciate feedback!
+    private HttpEntity<Transfer> makeEntity(Transfer transfer){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authenticatedUser.getToken());
+        return new HttpEntity<>(transfer, headers);
+    }
+
+
+}
