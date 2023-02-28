ea09f4b32cfb0cdfebfad27f3b5af1ba06c1de32
Aria <ariaodeli21@gmail.com>
2023-02-28 10:27:23 -0600

    added updates to TransferService

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -5,13 +5,15 @@ import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.util.BasicLogger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class TransferService {
-    private static final String API_BASE_URL = "http://localhost:8080/transfer";
+    private static final String API_BASE_URL = "http://localhost:8080/transfer/";
     private final RestTemplate restTemplate = new RestTemplate();
 
     private AuthenticatedUser authenticatedUser;
@@ -21,10 +23,23 @@ public class TransferService {
     }
 
     //method to get all past transfers. not sure if i did this right, i might add a test just to make sure.
-    public Transfer[] getPastTransfers(){
+    public Transfer[] getPastTransfers(int id){
         Transfer[] transfers = null;
         try {
-            transfers = restTemplate.getForObject(API_BASE_URL, Transfer[].class);
+            transfers = restTemplate.getForObject(API_BASE_URL + id, Transfer[].class);
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return transfers;
+    }
+
+    //method to get pending transfers
+    public Transfer[] getPendingTransfers(int id){
+        Transfer[] transfers = null;
+        try {
+            transfers = restTemplate.getForObject(API_BASE_URL + id, Transfer[].class);
         } catch (RestClientResponseException e) {
             BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
         } catch (ResourceAccessException e) {
