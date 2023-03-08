a5e53d3ac426b0c2b446f3f5b6af60867de3c1d2
DARSEA FINLEY <darseafinley@gmail.com>
2023-03-08 11:33:52 -0600

    getSentTransfersTest passes!!!! :)

--- tenmo-server/src/test/java/com/techelevator/dao/JdbcTransferDaoTests.java
+++ tenmo-server/src/test/java/com/techelevator/dao/JdbcTransferDaoTests.java
@@ -1,39 +1,46 @@
-//package com.techelevator.dao;
-//import com.techelevator.tenmo.dao.JdbcTransferDao;
-//import com.techelevator.tenmo.dao.JdbcUserDao;
-//import com.techelevator.tenmo.dao.TransferDao;
-//import com.techelevator.tenmo.model.Transfer;
-//import com.techelevator.tenmo.model.Transfer;
-//import com.techelevator.tenmo.model.User;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.dao.DataIntegrityViolationException;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringRunner;
+package com.techelevator.dao;
+import com.techelevator.tenmo.dao.JdbcTransferDao;
+import com.techelevator.tenmo.dao.JdbcUserDao;
+import com.techelevator.tenmo.dao.TransferDao;
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestingDatabaseConfig.class)
+public class JdbcTransferDaoTests extends BaseDaoTests {
+
+    protected static final Transfer TRANSFER_1 = new Transfer(3001, "Send", "Approved", 2002, 2001, new BigDecimal("15.00"));
+    protected static final Transfer TRANSFER_2 = new Transfer(3002, "Send", "Approved", 2001, 2002, new BigDecimal("12.00"));
+    private static final Transfer TRANSFER_3 = new Transfer(3003, "Send", "Approved", 2002, 2001, new BigDecimal("5.00"));
+    private JdbcTransferDao sut;
+
+    @Before
+    public void setup() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        sut = new JdbcTransferDao(jdbcTemplate);
+    }
+
+    @Test
+    public void getSentTransfers_returns_sent_transfers (){
+        List<Transfer> transfers = sut.getSentTransfers(TRANSFER_1.getTransferId());
+        Assert.assertNotNull(transfers);
+        Assert.assertEquals(transfers, transfers);
+    }
 //
-//
-//import java.math.BigDecimal;
-//import java.util.ArrayList;
-//import java.util.List;
-//@RunWith(SpringRunner.class)
-//@ContextConfiguration(classes = TestingDatabaseConfig.class)
-//public class JdbcTransferDaoTests extends BaseDaoTests {
-//
-//    protected static final Transfer TRANSFER_1 = new Transfer(3001, "Send", "Approved", 2002, 2001, new BigDecimal("15.00"));
-//    protected static final Transfer TRANSFER_2 = new Transfer(3002, "Send", "Approved", 2001, 2002, new BigDecimal("12.00"));
-//    private static final Transfer TRANSFER_3 = new Transfer(3003, "Send", "Approved", 2002, 2001, new BigDecimal("5.00"));
-//    private JdbcTransferDao sut;
-//
-//    @Before
-//    public void setup() {
-//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-//        sut = new JdbcTransferDao(jdbcTemplate);
-//    }
-////
 ////    @Test
 ////    public void updateTransfer_updates_correctly() {
 //
@@ -90,5 +97,5 @@
 ////        sut.updateTransfer(TRANSFER_1);
 ////    }
 ////
-//}
+}
 //
